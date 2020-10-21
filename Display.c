86cedd09d01082785a1c12beb47c05c17070b527
Tarik <47662299+tarikozluk@users.noreply.github.com>
2020-10-21 23:24:38 +0300

    Update Display.c

--- Display.c
+++ Display.c
@@ -18,38 +18,17 @@ void transmit(const char *str){
 
 const char yellow_screen[] = "cls YELLOW";
 const char dim[] = "dim=80";
-const char x1[] ="line 40,40,200,40,BLUE";
-const char x0[] ="line 40,40,40,360,BLUE";
-const char x20[] ="line 200,40,200,360,BLUE";
-const char x10[]="line 40,20,200,20,BLUE";
-const char x11[]="line 40,60,200,60,BLUE";
-const char x12[]="line 40,100,200,100,BLUE";
-const char x13[]="line 40,140,200,140,BLUE";
-const char x14[]="line 40,180,200,180,BLUE";
-const char x15[]="line 40,220,200,220,BLUE";
-const char x16[]="line 40,260,200,260,BLUE";
-const char x17[]="line 40,300,200,300,BLUE";
-const char x18[]="line 40,340,200,340,BLUE";
-const char x19[]="line 40,380,200,380,BLUE";
-const char x2[] ="line 40,80,200,80,BLUE";
-const char x3[] ="line 40,120,200,120,BLUE";
-const char x4[] ="line 40,160,200,160,BLUE";
-const char x5[] ="fill 40,199,160,3,RED";
-const char x6[]="line 40,240,200,240,BLUE";
-const char x7[]="line 40,280,200,280,BLUE";
-const char x8[]="line 40,320,200,320,BLUE";
-const char x9[]="line 40,360,200,360,BLUE";
-const char y1[]="line 40,0,40,400,BLUE";
-const char y2[]="line 80,40,80,360,BLUE";
-const char y3[]="fill 119,40,3,320,RED";
-const char y4[]="line 160,40,160,360,BLUE";
-const char y6[]="line 20,0,20,400,BLUE";
-const char y5[]="line 200,0,200,400,BLUE";
-const char y7[]="line 60,40,60,360,BLUE";
-const char y8[]="line 100,40,100,360,BLUE";
-const char y9[]="line 140,40,140,360,BLUE";
-const char y10[]="line 180,40,180,360,BLUE";
-const char y11[]="line 220,0,220,400,BLUE";
+
+//const char x1[] ="line 40,120,160,,BLUE";
+
+const char x0[] ="fill 40,199,160,3,RED";
+
+const char y0[]="fill 119,40,3,320,RED";
+
+//const char y2[]="line 120,40,0,320,BLUE";
+
+//Coordinates can be more and changed according to projects
+// line command for nextion editor: line x1,y1,x2,y2
 const char term_string[4] = {255,255,255,0};
 int main(void)
 {
@@ -63,8 +42,16 @@ int main(void)
     UCA0BR1 = 0;
     UCA0MCTL = UCBRS_0 + UCBRF_13 + UCOS16;
     UCA0CTL1 &= ~UCSWRST;
+       
+     
     transmit(yellow_screen);                   // clear screen
     transmit(term_string);
+       
+    transmit(y0);
+    transmit(term_string);
+    transmit(x0);
+    transmit(term_string);
+
 // Part for another installs according to projects with Nextion Display models
 // Install the necesseries in addition to this code
 }
